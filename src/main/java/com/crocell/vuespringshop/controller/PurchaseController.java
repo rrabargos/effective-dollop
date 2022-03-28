5913d813ee802891486b81d33052630c18266b0e
Rufino Rigor Abargos <rrabargos@gmail.com>
2022-03-28 15:50:40 +0800

    finalize endpoints

--- src/main/java/com/crocell/vuespringshop/controller/PurchaseController.java
+++ src/main/java/com/crocell/vuespringshop/controller/PurchaseController.java
@@ -8,10 +8,7 @@ import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +22,7 @@ public class PurchaseController {
     return new ResponseEntity<>(purchaseService.savePurchase(purchase), HttpStatus.CREATED);
   }
 
+  @GetMapping
   public ResponseEntity<?> getAllPurchaseOfUser (@AuthenticationPrincipal UserPrincipal userPrincipal){
     return ResponseEntity.ok(purchaseService.findPurchaseItemsOfUser(userPrincipal.getId()));
   }
